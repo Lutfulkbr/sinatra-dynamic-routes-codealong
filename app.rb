9f2efefa2127953e8139401f61fe6d292bc115b4
github username <github email address>
2021-02-08 12:45:49 -0500

    commit tree 651b8b6f43e8d0202c06e671240e736204fa3370

--- app.rb
+++ app.rb
@@ -12,7 +12,13 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
-
   # Code your final two routes here:
+  get "/goodbye/:name" do
+    erb :goodbye
+  end
+
+  get "/multiply/:num1/:num2" do
+    erb :multiply
+  end
 
 end
